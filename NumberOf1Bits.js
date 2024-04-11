7de0c9ac27039c3623e3f23f66f72cad9ab0c840
krispypatata <keithginoel@gmail.com>
2024-04-12 04:57:50 +0800

    easy leetcode problem

--- NumberOf1Bits.js
+++ NumberOf1Bits.js
@@ -29,3 +29,42 @@ Constraints:
     1 <= n <= 231 - 1
 
 */
+
+
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var hammingWeight = function(n) {
+    // will store the converted binary representation of the given number n
+    let binaryRepresentation = "";
+
+    // convert the number n to binary
+    while (n/2 != 0) {
+        // get the remainder and append it to the binary representation variable
+        binaryRepresentation += n%2;
+
+        // integer division
+        n = Math.floor(n/2);
+    }
+
+    // count the number of 1s in the binary representation
+    let count = 0;
+    for (let i = 0; i<binaryRepresentation.length; i++) {
+        if (binaryRepresentation[i]==='1') {
+            count++;
+        }
+    }
+
+    return count;
+};
+
+
+// sample run
+const sampleInput1 = 11;
+const sampleInput2 = 128;
+const sampleInput3 = 2147483645;
+
+console.log(hammingWeight(sampleInput1)); // 3
+console.log(hammingWeight(sampleInput2)); // 1
+console.log(hammingWeight(sampleInput3)); // 30
