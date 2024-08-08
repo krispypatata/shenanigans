ac5d2eba6d4b8928398f5954deafcfc7c8627678
krispypatata <keithginoel@gmail.com>
2024-08-09 00:08:01 +0800

    Revised the solution for more optimal performance

--- LargestPerimeterTriangle.js
+++ LargestPerimeterTriangle.js
@@ -1,6 +1,7 @@
 /*
 author: Keith Ginoel Gabinete
 created: 03 August 2024 23:35:41
+revised: 09 August 2024 00:07:15
 */
 /*
 https://leetcode.com/problems/largest-perimeter-triangle/
@@ -32,49 +33,38 @@ Constraints:
 */
 
 
-
 /**
  * @param {number[]} nums
  * @return {number}
  */
 var largestPerimeter = function(nums) {
-    
-    const possibleSideLengths = [];
+    // sort the array in non-decreasing order
+    // we do this so that we can easily find the largest perimeter by checking if the largest 3 consecutive elements of the array can form a triangle
+    // if it can, then that means their sum is the largest possible perimeter of a triangle
+    // however, it the largest 3 consecutive elements cannot form a triangle, then we can move to the next largest 3 consecutive elements
+    // we do this until we find the largest possible perimeter of a triangle
+    // if we cannot find any 3 consecutive elements that can form a triangle, then we can simply return 0 - indicating that it is impossible to form a triangle with the given array
+    nums.sort((a, b) => a - b);
 
-    for (let i = 0; i < nums.length - 2; i++) {
-        for (let j = i + 1; j < nums.length - 1; j++) {
-            for (let k = j + 1; k < nums.length; k ++) {
-                possibleSideLengths.push([nums[i], nums[j], nums[k]]);
-            }
+    let maxPerimeter = 0; // initialize the variable that will hold the largest perimeter of a triangle
+
+    // loop through the array starting from the last element (largest possible side of a triangle)
+    for (let i = nums.length - 1; i >= 2; i--) {
+        // get the current largest 3 consecutive elements
+        const a = nums[i - 2];
+        const b = nums[i - 1];
+        const c = nums[i];
+        
+        // check if the 3 consecutive elements can form a triangle
+        // if it is, then we can just simply get their sum and that will be the largest perimeter of a triangle
+        if ((a + b) > c) {
+            maxPerimeter = a + b + c;
+            break; // stop the loop since we already found the largest possible perimeter of a triangle (since, again, the array is sorted in non-decreasing order)
         }
     }
 
-    // console.log(possibleSideLengths);
-
-
-    let maxPerimeter = 0;
-    possibleSideLengths.forEach((sublist) => {
-        let isValid = true;
-
-        [a, b, c] = sublist;
-        
-        if ( (a + b) <= c || (a + c) <= b || (b + c) <= a) {
-            // console.log(a + ' + ' + b + ' <= ' + c);
-            isValid = false;
-        }
-
-        if (isValid) {
-            const perimeter = a + b + c;
-            if (perimeter > maxPerimeter)
-                maxPerimeter = perimeter;
-        }
-
-
-    });
-
+    // return the result
     return maxPerimeter;
-
-
 };
 
 
@@ -84,3 +74,6 @@ console.log('Output:', largestPerimeter([2,1,2]));
 console.log();
 console.log('Input: nums = [1,2,1,10]'); // expected output: 0
 console.log('Output:', largestPerimeter([1,2,1,10]));
+console.log()
+console.log('Input: nums = [2, 3, 3, 6]');; // expected output = 8
+console.log('Output:', largestPerimeter([ 2,3,3,6 ]));
